0da44754768861b95b3cecd8e0f18230e48ac236
Loli Admin <Loli Admin@DESKTOP-H4SR9KC>
2025-04-04 12:06:31 +0300

    Добавьте файлы проекта.

---
+++ N5__YP__Task-7_04.04.2025/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace N5__YP__Task_7_04._04._2025
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите размер массива n: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] B = new int[n];
+
+            Console.WriteLine("Введите элементы массива:");
+            for (int i = 0; i < n; i++) B[i] = int.Parse(Console.ReadLine());
+
+            int countInInterval = 0, countDivBy5 = 0;
+
+            foreach (var num in B)
+            {
+                countInInterval += num >= -7 && num <= 7 ? 1 : 0;
+                countDivBy5 += num % 5 == 0 ? 1 : 0;
+            }
+
+            Console.WriteLine($"Количество элементов в интервале [-7; 7]: {countInInterval}");
+            Console.WriteLine($"Количество элементов, кратных 5: {countDivBy5}");
+        }
+    }
+}
